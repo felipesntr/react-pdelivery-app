09d7f0e144e93ba0ce013219ccb64ed261d5f376
Felipe Santos <rfelipe273@gmail.com>
2022-01-02 14:22:37 -0300

    add home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css';
 import { Navbar } from './presentation/components/navbar';
+import { Home } from './presentation/pages/home/Home';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Home />
     </div>
   );
 }
